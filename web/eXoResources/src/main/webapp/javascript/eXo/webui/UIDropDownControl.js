450322df5f38039c53b52e224b6df20e2fb29dc0
phuong_vu <phuong.vu@exoplatform.com>
2012-11-23 10:20:35 +0700

    Use bootstrap js for dropdown control

--- web/eXoResources/src/main/webapp/javascript/eXo/webui/UIDropDownControl.js
+++ web/eXoResources/src/main/webapp/javascript/eXo/webui/UIDropDownControl.js
@@ -19,17 +19,10 @@
 
 eXo.webui.UIDropDownControl = {
 
-  init : function(id, webui)
+  init : function(id)
   {
     var elmt = $('#' + id);
-    elmt.find('.UIDropDownTitle').on('click', this.showEvt);
     elmt.find('a.OptionItem').on('click', this.onclickEvt);
-    elmt.find(".TopItemContainer").on('mousedown', function() {
-    	webui.VerticalScrollManager.initScroll(this, true, 10);
-    });   
-    elmt.find(".BottomItemContainer").on('mousedown', function() {
-    	webui.VerticalScrollManager.initScroll(this, false, 10);
-    });
   },
 
   selectItem : function(method, id, selectedIndex) {
@@ -37,74 +30,6 @@ eXo.webui.UIDropDownControl = {
       method(id, selectedIndex);
   },
 
-  /*
-   * . minh.js.exo
-   */
-  /**
-   * show or hide drop down control
-   * 
-   * @param {Object}
-   *          obj document object to use as Anchor for drop down
-   * @param {Object}
-   *          evet event object
-   */
-  showEvt : function() {
-    var dropDownAnchor = $(this).next("div");
-    if(!dropDownAnchor)
-    {
-      return;
-    }
-
-    if(dropDownAnchor.css("display") == "none")
-    {
-      dropDownAnchor.css({"display" : "block" , "visibility" : "visible"});
-
-      var middleCont = dropDownAnchor.find("div.MiddleItemContainer");
-      var topCont = middleCont.prev("div");
-      var bottomCont = middleCont.next("div");
-
-      topCont.css("display", "block");
-      bottomCont.css("display", "block");
-
-      var visibleHeight = $(window).height() - $(middleCont[0]).offset().top - 40;
-      if(middleCont[0].scrollHeight > visibleHeight)
-      {
-        middleCont.css("height", visibleHeight - topCont[0].offsetHeight - bottomCont[0].offsetHeight + "px");
-        topCont.click(function() { return false;});
-        bottomCont.click(function() { return false;});
-      }
-      else
-      {
-        topCont.css("display", "none");
-        bottomCont.css("display", "none");
-        middleCont.scrollTop(0);
-        middleCont.css("height", "auto");
-      }
-
-      $(document).one("click", function()
-      {
-    	  $(document).one("click", function()
-    	  {
-	        dropDownAnchor.css("display", "none");
-    	  });
-      });
-    }
-    else
-    {
-      dropDownAnchor.css({"display" : "none" , "visibility" : "hidden"});
-    }
-  },
-  /**
-   * Hide an object
-   * 
-   * @param {Object,
-   *          String} obj object to hide
-   */
-  hide : function(obj) {
-    if (typeof (obj) == "string")
-      obj = document.getElementById(obj);
-    obj.style.display = "none";
-  },
   /**
    * Use as event when user selects a item in drop down list Display content of
    * selected item and hide drop down control
@@ -113,11 +38,9 @@ eXo.webui.UIDropDownControl = {
    *          obj selected object
    */
   onclickEvt : function() {
-    var dropDownAnchor = $(this).parents("div.UIDropDownAnchor");
-    var dropDownMiddleTitle = dropDownAnchor.prev("div.UIDropDownTitle").find("div.DropDownSelectLabel");
-    dropDownMiddleTitle.html($(this).html());
-
-    dropDownAnchor.css("display", "none");
+    var dropdown = $(this).closest(".dropdown-menu").parent();
+    var title = dropdown.find(".dropdown-toggle > span").first();
+    title.html($(this).html());
   }
 };
 
