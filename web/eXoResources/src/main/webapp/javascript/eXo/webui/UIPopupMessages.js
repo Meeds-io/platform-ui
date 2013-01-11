9c49db33121da12eca649206dc95577e9886ed1b
phuong_vu <phuong.vu@exoplatform.com>
2013-01-11 11:26:12 +0700

    UI-2374: [PLF 4] apply js for popup messages

---
+++ web/eXoResources/src/main/webapp/javascript/eXo/webui/UIPopupMessages.js
@@ -0,0 +1,41 @@
+/**
+ * Copyright (C) 2013 eXo Platform SAS.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+/**
+ * A class that manages a popup message
+ */
+var uiPopupMessages = {
+	init : function(popupId) {
+		var collapsible = $("#" + popupId + " *[data-toggle='collapse']");
+		collapsible.parent().on("show hide", function() {
+			$(this).find("i").toggleClass("uiIconArrowRight uiIconArrowDown");
+		});
+	},
+	
+  show : function(popupId, isShowMask) {
+  	var popupWindow = webui.UIPopupWindow;
+  	popupWindow.show(popupId, isShowMask);  	
+  },
+  
+  hide : function(popupId, isShowMask) {
+  	var popupWindow = webui.UIPopupWindow;
+  	popupWindow.hide(popupId, isShowMask);
+  }
+};
+_module = uiPopupMessages;
