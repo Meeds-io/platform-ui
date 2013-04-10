c70d9573855d72949043cea9266662885e2a4c01
trung_nguyenthanh <trungnt@exoplatform.com>
2013-04-10 12:10:24 +0700

    UI-2664: improvement stylesheet

--- platform-ui-web-eXoResources/src/main/webapp/javascript/eXo/webui/UICalendar.js
+++ platform-ui-web-eXoResources/src/main/webapp/javascript/eXo/webui/UICalendar.js
@@ -73,11 +73,11 @@
 			clndr.style.position = "absolute";
 			clndr.style.zIndex = "99";
 	    if (base.Browser.isIE6()) {
-	      clndr.innerHTML = "<div class='calendarComponent' style='position: absolute;'><iframe id='"
+	      clndr.innerHTML = "<div class='calendarComponent' ><iframe id='"
 	          + this.calendarId
 	          + "IFrame' frameBorder='0' style='position:absolute;height:100%;' scrolling='no'></iframe><div style='position:absolute;'></div></div>";
 	    } else {
-	      clndr.innerHTML = "<div class='calendarComponent' style='position: absolute;''><div style='position: absolute; width: 100%;'></div></div>";
+	      clndr.innerHTML = "<div class='calendarComponent' ><div style='position: absolute; width: 100%;'></div></div>";
 	    }
 	    document.body.appendChild(clndr);
 	  },
